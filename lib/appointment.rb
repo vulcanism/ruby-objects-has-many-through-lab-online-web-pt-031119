596d36daf70cab6bfd163526067730b36b8997f9
Savannah C <sgcorrell@gmail.com>
2019-04-11 18:29:27 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,7 +4,10 @@ class Appointment
   @@all = []
   
   def initialize(date, patient, doctor)
-    
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
   end
   
   def self.all
